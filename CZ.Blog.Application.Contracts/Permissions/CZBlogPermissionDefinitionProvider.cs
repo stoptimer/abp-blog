275c05bdb952b30553fcbceafe901ba2f25aa758
xyc309 <xyc309@gmail.com>
2020-08-25 17:46:19 +0800

    add CZBlogPermissions

---
+++ CZ.Blog.Application.Contracts/Permissions/CZBlogPermissionDefinitionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Authorization.Permissions;
+
+namespace CZ.Blog.Application.Contracts.Permissions
+{
+    public class CZBlogPermissionDefinitionProvider : PermissionDefinitionProvider
+    {
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var myGroup = context.AddGroup(CZBlogPermissions.GroupName);
+        }
+    }
+}
---
+++ CZ.Blog.Application.Contracts/Permissions/CZBlogPermissions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CZ.Blog.Application.Contracts.Permissions
+{
+    public class CZBlogPermissions
+    {
+        public const string GroupName = "BlogMaster";
+    }
+}
